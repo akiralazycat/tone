e83507a0e1fde4d5700fa2e016b10f2aacb38adc
akira <akiraloafpanda@icloud.com>
2026-08-23 11:22:10 +0900

    Expose Brand Reverse and Compare Lab on home

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,13 @@
+import LabsFooter from "@/components/labs-footer";
+import ToneLabs from "@/components/tone-labs";
 import ToneStudio from "@/components/tone-studio";
 
 export default function Home() {
-  return <ToneStudio />;
+  return (
+    <>
+      <ToneStudio />
+      <ToneLabs />
+      <LabsFooter />
+    </>
+  );
 }
